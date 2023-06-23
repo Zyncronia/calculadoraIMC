126715c8749845ca9e629a8ade52f00b8f94a47e
Julia Yumi <juliayumifranz@gmail.com>
2023-06-22 22:05:50 -0300

    js started

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
-function calculaIMC(resp_altura, resp_peso) {
+function calculaIMC(){
 
+    var resp_altura = document.getElementById("resp_altura").value
+    var resp_peso = document.getElementById("resp_peso").value
+    var imc = 0;
+    let  verificaIMC;    
+    
+    imc = resp_peso / (resp_altura * resp_altura)
+
+    console.log(imc)
+
+
+    if(imc <= 18.5){
+        document.getElementById("msgRetorno").innerHTML = "<span>Seu IMC é: </span>"+ imc + "<br><span>IMC baixo</span>";
+    }else if((imc > 18.5) && (imc <= 25)){
+        document.getElementById("msgRetorno").innerHTML = "<span>Seu IMC é: </span>"+ imc + "<br><span>IMC normal</span>";
+    }
+
+
+
+    // switch (imc) {
+    //     case imc < 18.5:
+    //         document.getElementById("msgRetorno").innerHTML = "<span>Seu IMC é: </span>"+ number + "<br><span>IMC baixo</span>";
+    //     break;
+    //     case imc < 25:
+    //         document.getElementById("msgRetorno").innerHTML = "<span>Seu IMC é: </span>";
+    //         document.getElementById("msgRetorno").value = imc;
+    //         document.getElementById("msgRetorno").innerHTML = "<span>IMC normal</span>";
+    //     break;
+    //     default:
+    //         document.getElementById("msgRetorno").innerHTML = "<span>Se deu mal</span>";
+    //     break;
+    // }
 }
+
