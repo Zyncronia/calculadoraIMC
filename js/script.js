31a800a590a4cea09f9bb227ea1c2258d657a121
Julia Yumi <juliayumifranz@gmail.com>
2023-06-28 21:01:32 -0300

    colocado fundo gradiente  e css

--- js/script.js
+++ js/script.js
@@ -12,11 +12,11 @@ function calculaIMC(){
 
     if(imc <= 18.5){
         document.getElementById("msgRetorno").innerHTML = "<span>Seu IMC é: </span>"+ imc + "<span> e seu peso está na categoria: Baixo</span>";
-    }else if (imc <= 24.9){
+    }else if ((imc > 18.5) && (imc <= 24.9)){
         document.getElementById("msgRetorno").innerHTML = "<span>Seu IMC é: </span>"+ imc + "<span> e seu peso está na categoria: Normal</span>";
-    }else if(imc<=29.9){
+    }else if((imc > 25.0)&& (imc <= 29.9)){
         document.getElementById("msgRetorno").innerHTML = "<span>Seu IMC é: </span>"+ imc + "<span> e seu peso está na categoria: Acima do peso</span>";
-    }else if(imc<=39.9){
+    }else if((imc > 30)&&(imc<=39.9)){
         document.getElementById("msgRetorno").innerHTML = "<span>Seu IMC é: </span>"+ imc + "<span> e seu peso está na categoria: Obesidade I</span>";
     }else{
         document.getElementById("msgRetorno").innerHTML = "<span>Seu IMC é: </span>"+ imc + "<span> e seu peso está na categoria: Obesidade II</span>";
